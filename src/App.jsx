89e95addf1483e28a6c7f21dcf732b2faa00c5e5
Jhancarlo Esteban <estebanjcarlo@gmail.com>
2023-07-11 16:43:21 -0500

    avance cuerpo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { jsPDF } from "jspdf";
+import { jsPDF } from "jspdf/dist/jspdf.umd.min.js";
 
 function App() {
    
 // Crear instancia de jsPDF
 const doc = new jsPDF();
-doc.addFont("times.ttf", "times", "normal", "WinAnsiEncoding");
+doc.addFont("times", "normal", "WinAnsiEncoding");
 // Definir el estilo de fuente
 doc.setFont("times", "bold");
 
@@ -22,27 +22,41 @@ doc.text(`"Alma Máter del Magisterio Nacional"`, doc.internal.pageSize.getWidth
 doc.setFontSize(14);
 doc.setFont("times", "bold");
 doc.text("RECTORADO", doc.internal.pageSize.getWidth() / 2, 35, { align: "center" });
-
 //Añadir imagen 
 let imgData = "https://upload.wikimedia.org/wikipedia/commons/0/08/Escudo_UNE.png";
 doc.addImage(imgData, "PNG", 102, 36, 8, 12, { align: "center" });
-
-
 //Añadir linea
 doc.setLineWidth(0.5);
 doc.line(20, 50, 190, 50); 
 
 
-
 doc.setFontSize(12);
-doc.setFont("times");
+doc.setFont("times", "normal");
 doc.text("Hoja de Envío N°: 1612-2023-R-UNE", doc.internal.pageSize.getWidth() / 2, 58, { align: "center" });
 
+doc.setFontSize(12);
+doc.setFont("times", "normal");
+doc.text("FECHA: 22 junio", 20, 70);
 
+doc.setFontSize(12);
+doc.setFont("times", "normal");
+doc.text("N° DE FOLIOS: 03", 130, 70);
 
+doc.setFontSize(12);
+doc.setFont("times", "normal");
+doc.text("DOCUMENTO: OFICIO No 2001-2023-MINEDU/VMGI-DRELM-UGEL05-ARH.", 20, 80);
 
+doc.setFontSize(12);
+doc.setFont("times", "normal");
+doc.text("MPV-EPP-740-23.", 20, 85);
 
+doc.setFontSize(12);
+doc.setFont("times", "normal");
+doc.text("REMITIDO POR: Área de Recursos Humanos - UGEL 05", 20, 95);
 
+doc.setFontSize(12);
+doc.setFont("times", "normal");
+doc.text("ASUNTO: Procedimiento de fiscalización posterior - Carmen Rosa Castillo Ojeda", 20, 105);
 
 
 // Guardar el PDF
